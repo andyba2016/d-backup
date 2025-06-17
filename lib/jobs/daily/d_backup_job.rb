76de160dd7abef93de61c5dd1454c8fcc06ca3b1
andyba2016 <andresm8tarrio@yahoo.com.ar>
2025-06-17 12:42:42 -0300

    update cron

--- lib/jobs/daily/d_backup_job.rb
+++ lib/jobs/daily/d_backup_job.rb
@@ -1,36 +1,21 @@
-# name: d_backup
-# about: Custom backup plugin with cron control
-# version: 0.1
+# lib/jobs/daily/d_backup_job.rb
 
-module ::Jobs
+module Jobs
   class DBackupJob < ::Jobs::Scheduled
-    every 1.day
+    every 10.minutes
 
     def execute(args)
-      return unless SiteSetting.d_backup_enable_daily_job
+      return unless SiteSetting.d_backup_enabled
 
-      Rails.logger.info("[DBackupJob] Starting daily backup job...")
-     
-     source_dir = Rails.root.join("public", "backups", "default").to_s
-     plugin_setting = SiteSetting.d_backup_dest_dir
-     dest_dir = Rails.root.join(plugin_setting).to_s   
+      Rails.logger.info("[DBackupJob] Running external backup script...")
 
-      # Aquí llamas al código real de tu backup
-      user = Discourse.system_user
-      backup = CustomBackup::LocalBackuper.new(user.id, with_uploads: true)
-      filename = backup.run
+      script_path = Rails.root.join("script", "run_local_backup.rb").to_s
+      result = `bundle exec ruby #{script_path} 2>&1`
 
-      if backup.success && filename
-          source_file = File.join(source_dir, filename)
-          dest_file = File.join(dest_dir, filename)
-            if File.exist?(source_file)
-                FileUtils.mv(source_file, dest_file)
-                puts "[SUCCESS] Backup completed successfully: #{dest_file}"
-            else
-                puts "[ERROR] Backup file not found: #{source_file}"
-            end
+      if $?.success?
+        Rails.logger.info("[DBackupJob] Script completed successfully:\n#{result}")
       else
-        Rails.logger.warn("[DBackupJob] Backup failed or did not return a filename.")
+        Rails.logger.error("[DBackupJob] Script failed:\n#{result}")
       end
     end
   end
